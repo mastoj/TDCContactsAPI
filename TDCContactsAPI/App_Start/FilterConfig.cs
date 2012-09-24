bcac1772df80e484c023b682e122eb1b1cf7a2e2
Tomas Jansson <tomas.jansson@gmail.com>
2012-09-24 12:12:21 +0200

    added validation

--- TDCContactsAPI/App_Start/FilterConfig.cs
+++ TDCContactsAPI/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using TDCContactsAPI.Filters;
 
 namespace TDCContactsAPI
 {
@@ -8,6 +9,7 @@ namespace TDCContactsAPI
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new ValidationActionFilter());
         }
     }
 }
