fb4a0101761665430124f957b1ccf55e1f3d15d0
Tomas Jansson <tomas.jansson@gmail.com>
2012-09-24 11:58:02 +0200

    added CRUD

--- TDCContactsAPI/Controllers/ContactsController.cs
+++ TDCContactsAPI/Controllers/ContactsController.cs
@@ -10,31 +10,55 @@ namespace TDCContactsAPI.Controllers
 {
     public class ContactsController : ApiController
     {
+        private ContactRepository _contactsRepository;
+
+        public ContactsController()
+        {
+            _contactsRepository = new ContactRepository();
+        }
+
         // GET api/values
         public IEnumerable<Contact> Get()
         {
-            return new[] { Contact.CreateContact(1, firstName: "John"), Contact.CreateContact(2, firstName: "Tomas") };
+            return _contactsRepository.Get();
         }
 
         // GET api/values/5
         public Contact Get(int id)
         {
-            return Contact.CreateContact(1, firstName: "John");
+            return _contactsRepository.Get().SingleOrDefault(y => y.Id == id);
         }
 
         // POST api/values
         public void Post([FromBody]Contact value)
         {
+            _contactsRepository.Insert(value);
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]Contact value)
+        public HttpResponseMessage Put(int id, [FromBody]Contact value)
         {
+            var contact = _contactsRepository.Get().SingleOrDefault(y => y.Id == id);
+            if (contact != null)
+            {
+                contact.FirstName = value.FirstName;
+                contact.LastName = value.LastName;
+                contact.PhoneNumber = value.PhoneNumber;
+                return Request.CreateResponse(HttpStatusCode.Accepted, contact);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            var contact = _contactsRepository.Get().SingleOrDefault(y => y.Id == id);
+            if (contact != null)
+            {
+                _contactsRepository.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.Accepted);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
     }
 }
